47e19ea8585bf8bac379ce3a0827954f5f97681b
Sarthakjain714 <jainsarthak6021@gmail.com>
2023-03-22 21:53:27 +0530

    final Commit

--- app/src/main/java/com/example/ipaddress4/MainActivity.java
+++ app/src/main/java/com/example/ipaddress4/MainActivity.java
@@ -4,6 +4,7 @@ import static androidx.core.content.ContentProviderCompat.requireContext;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.app.DownloadManager;
 import android.content.Context;
 import android.content.Intent;
 import android.net.wifi.WifiInfo;
@@ -16,78 +17,98 @@ import android.view.View;
 import android.widget.Button;
 import android.widget.EditText;
 import android.widget.TextView;
+import android.widget.Toast;
 
+import com.android.volley.Request;
+import com.android.volley.RequestQueue;
+import com.android.volley.Response;
+import com.android.volley.VolleyError;
+import com.android.volley.toolbox.JsonObjectRequest;
+import com.android.volley.toolbox.Volley;
+import com.google.gson.Gson;
+
+import org.json.JSONException;
+import org.json.JSONObject;
+import org.w3c.dom.Document;
+import org.w3c.dom.Element;
+import org.w3c.dom.NodeList;
+import org.xml.sax.SAXException;
+
+import java.io.IOException;
+import java.io.InputStream;
+import java.net.HttpURLConnection;
 import java.net.Inet4Address;
 import java.net.InetAddress;
+import java.net.MalformedURLException;
 import java.net.NetworkInterface;
 import java.net.SocketException;
+import java.net.URL;
+import java.net.URLConnection;
 import java.net.UnknownHostException;
 import java.util.Enumeration;
 
+import javax.xml.parsers.DocumentBuilder;
+import javax.xml.parsers.DocumentBuilderFactory;
+import javax.xml.parsers.ParserConfigurationException;
+
 public class MainActivity extends AppCompatActivity {
-    EditText email, password;
-    Button submit;
+
     TextView loginkrnahai;
-    String ip;
+
+    public final String STATE_LEVEL="SarthakJain";
+
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
-        email=findViewById(R.id.email);
-        password=findViewById(R.id.password);
-        submit=findViewById(R.id.submit);
-        loginkrnahai=findViewById(R.id.loginkrnahai);
-        loginkrnahai.setOnClickListener(new View.OnClickListener() {
-            @Override
-            public void onClick(View view) {
-                Intent intent=new Intent(getApplicationContext(),login.class);
-                startActivity(intent);
-                finish();
-            }
-        });
-        ip=findip.getip();
-        Log.d("ipaddress", "onCreate: "+ip);
-        loginkrnahai.setText(ip);
+        loginkrnahai = findViewById(R.id.loginkrnahai);
 
-//        WifiManager manager=(WifiManager) getApplicationContext().getSystemService(WIFI_SERVICE);
-//        ip=Formatter.formatIpAddress(manager.getConnectionInfo().getIpAddress());
+        String url = getString(R.string.server_url_Myip);
+        RequestQueue queue = Volley.newRequestQueue(this);
+        JsonObjectRequest jsonObjectRequest = new JsonObjectRequest
+                (Request.Method.GET, url, null, new Response.Listener<JSONObject>() {
+
+                    @Override
+                    public void onResponse(JSONObject response) {
+
+                        Gson gson = new Gson();
+
+                        String dataArray = null;
+
+                        try {
+                            dataArray = response.getString("ip");
+                        } catch (JSONException e) {
+                            Log.e(this.getClass().toString(), e.getMessage());
+                        }
 
 
-//        StrictMode.ThreadPolicy gfgPolicy = new StrictMode.ThreadPolicy.Builder().permitAll().build();
-//        StrictMode.setThreadPolicy(gfgPolicy);
-//        byte[] ip = new byte[0];
-//        try {
-//            InetAddress address = InetAddress.getLocalHost();
-//            ip = address.getAddress();
-//        } catch (UnknownHostException e) {
-//            e.printStackTrace();
-//        }
-//        String ipAddress = getIpAddress(ip);
-//        System.out.println("IP Address = " + ipAddress);
-//
-//        try {
-//            InetAddress address = InetAddress.getByName("google.com");
-//            ip = address.getAddress();
-//        } catch (UnknownHostException e) {
-//            e.printStackTrace();
-//        }
-//        ipAddress = getIpAddress(ip);
-//        System.out.println("IP Address = " + ipAddress);
-////        ip=getIpAddress();
-//        loginkrnahai.setText(""+ipAddress);
 
+                        loginkrnahai.setText(dataArray);
+                    }
+                }, new Response.ErrorListener() {
+
+                    @Override
+                    public void onErrorResponse(VolleyError error) {
+                        loginkrnahai.setText("Error! " + error.toString());
+                    }
+                });
+
+        queue.add(jsonObjectRequest);
     }
 
-//    private static String getIpAddress(byte[] rawBytes) {
-//        int i = 4;
-//        StringBuilder ipAddress = new StringBuilder();
-//        for (byte raw : rawBytes) {
-//            ipAddress.append(raw & 0xFF);
-//            if (--i > 0) {
-//                ipAddress.append(".");
-//            }
-//        }
-//        return ipAddress.toString();
-//    }
+
+
+    @Override
+    public void onSaveInstanceState(Bundle outState) {
+        outState.putString(STATE_LEVEL,loginkrnahai.getText().toString());
+        super.onSaveInstanceState(outState);
+    }
+
+    @Override
+    public void onRestoreInstanceState(Bundle savedInstanceState) {
+        loginkrnahai.setText(savedInstanceState.getString(STATE_LEVEL));
+    }
+
 
 }
+
